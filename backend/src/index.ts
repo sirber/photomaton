bdedc58e1e01deffc28638d128f749e404187927
Stephane Berube <sirber83@gmail.com>
2025-11-19 17:47:03 -0500

    added: connection to mongodb

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,13 @@
 import express from "express";
+import { connectToDatabase } from "./common/database";
 
 const app = express();
 const port = process.env.PORT || 3001;
+
+// Database
+await connectToDatabase();
+
+// API endpoints
 
 app.get("/", (req, res) => {
 	res.send("Hello, world!");
